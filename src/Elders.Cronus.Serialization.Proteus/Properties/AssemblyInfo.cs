b71efbf5c7fd25bd9dfb6df86903e19393ba4253
mynkow <mynkow@gmail.com>
2016-04-13 21:33:01 +0300

    Update Proteus and Cronus packages

--- src/Elders.Cronus.Serialization.Proteus/Properties/AssemblyInfo.cs
+++ src/Elders.Cronus.Serialization.Proteus/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Runtime.InteropServices;
 [assembly: ComVisibleAttribute(false)]
 [assembly: AssemblyProductAttribute("Elders.Cronus.Serialization.Proteus")]
 [assembly: AssemblyCopyrightAttribute("Copyright ©  2016")]
-[assembly: AssemblyVersionAttribute("2.0.3.0")]
-[assembly: AssemblyFileVersionAttribute("2.0.3.0")]
-[assembly: AssemblyInformationalVersionAttribute("2.0.3+Branch.master.Sha.f23b0bf84b95352fb080177b104a0d86a58602e0")]
+[assembly: AssemblyVersionAttribute("2.0.4.0")]
+[assembly: AssemblyFileVersionAttribute("2.0.4.0")]
+[assembly: AssemblyInformationalVersionAttribute("2.0.4+1.Branch.master.Sha.41d9351adadfabbea5f17cfc03a9adf6d549a3a5")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "2.0.3.0";
+        internal const string Version = "2.0.4.0";
     }
 }
